17dc50a921f0528dac2eff98ac8b0246015258d5
Lucas Aguiar <lucasaguiar.98@outlook.com>
2025-08-04 15:40:41 -0300

    Projeto de portifólio

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// INCIAR BIBLIOTECA DE ANIMAÇÕES
+AOS.init();
+
+// BOTÕES
+const menuHamburguer = document.querySelector(".btn-menu");
+const btnCloseMenu = document.querySelector(".btn-close-menu");
+const botoesInfo = [...document.querySelectorAll(".btn-info")];
+const btnCloseModal = [...document.querySelectorAll(".btn-close-modal")];
+const btnForm = document.querySelector(".btn-enviar-form");
+
+// MENU
+const sidebar = document.querySelector(".sidebar");
+
+
+menuHamburguer.addEventListener("click", ()=> {
+    sidebar.classList.remove("hidden");
+    document.documentElement.style.overflowY="hidden";   
+})
+
+btnCloseMenu.addEventListener("click", ()=> {
+    sidebar.classList.add("hidden");
+    document.documentElement.style.overflowY = "auto";
+})
+
+botoesInfo.forEach(button => {
+    button.addEventListener("click", (evt)=>{
+        const el = evt.target;
+        const modalCard = el.parentNode.nextElementSibling;
+        modalCard.classList.remove("hidden");
+    })
+})
+
+btnCloseModal.forEach(button => {
+    button.addEventListener("click", (evt)=>{
+        const el = evt.target.parentNode;
+        const modalCard = el.parentNode.parentNode;
+        modalCard.classList.add("hidden");
+    })
+})
+
+
